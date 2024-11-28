9327f84ab4dd52159bfb2de2b6d7f247a892e0b7
YouGamesDiaz <progmarcosdiaz2022@gmail.com>
2024-11-27 20:57:39 -0500

    modify: interface de reconocimiento de rostros

--- Frontend/js/main.js
+++ Frontend/js/main.js
@@ -1,73 +1,68 @@
-document.addEventListener("DOMContentLoaded", () => {
-    // Inicializar estado oculto
-    document.getElementById("loading").classList.add("hidden");
-    document.getElementById("result").classList.add("hidden");
+document.addEventListener('DOMContentLoaded', () => {
+  // Inicializar estado oculto
+  document.getElementById('loading').classList.add('hidden');
+  document.getElementById('result').classList.add('hidden');
 });
 
-document.getElementById("upload-form").addEventListener("submit", async (e) => {
-    e.preventDefault();
+document.getElementById('upload-form').addEventListener('submit', async (e) => {
+  e.preventDefault();
 
-    const formContainer = document.getElementById("form-container");
-    const loadingDiv = document.getElementById("loading");
-    const resultDiv = document.getElementById("result");
-    const imageResultDiv = document.getElementById("image-result");
-    const videoResultDiv = document.getElementById("video-result");
-    const previewImage = document.getElementById("preview-image");
+  const loadingDiv = document.getElementById('loading');
+  const loadingSpinner = document.getElementById('loader');
+  const loadingModal = document.getElementById('loading-modal');
+  const loadingModalTexts = document.getElementById('loading-modal-texts');
 
-    const formData = new FormData();
-    const image = document.getElementById("image").files[0];
-    const video = document.getElementById("video").files[0];
+  const resultDiv = document.getElementById('result');
+  const imageResultDiv = document.getElementById('image-result');
+  const videoResultDiv = document.getElementById('video-result');
+  const previewImage = document.getElementById('preview-image');
+
+  const formData = new FormData();
+  const image = document.getElementById('image').files[0];
+  const video = document.getElementById('video').files[0];
+
+  if (!image || !video) {
+    alert('Por favor, selecciona una imagen y un video antes de subir.');
+    return;
+  }
+
+  formData.append('imagen', image);
+  formData.append('video', video);
+
+  // Mostrar la imagen cargada en el contenedor de carga
+  previewImage.src = URL.createObjectURL(image);
+
+  loadingDiv.classList.remove('hidden');
+  loadingSpinner.classList.remove('hidden');
+
+  try {
+    const response = await fetch('https://rostros.buho.media/upload/', {
+      method: 'POST',
+      body: formData,
+    });
+
+    const result = await response.json();
+
+    // Ocultar el contenedor de carga y mostrar los resultados
+    loadingSpinner.classList.add('hidden'); // ocultaro loader
+
+    setTimeout(() => {
+      loadingModal.classList.remove('hidden'); // mostrar modal
+    }, 2000);
+
+    loadingModalTexts.innerHTML = `
+    <p>Ubicación de la cara: ${JSON.stringify(
+      result.imagen.ubicaciones_caras
+    )}</p>
+
+    <p>Coincidecias detectadas: ${result.imagen.numero_caras_detectadas}</p>
     
-    console.log({formContainer})
-    console.log("loading" + loadingDiv)
-    console.log("result" + resultDiv)
-    console.log("image" + imageResultDiv)
-    console.log("video" + videoResultDiv)
-    console.log("preview" + previewImage)
-    console.log("image" + image)
-    console.log("video" + video)
+    `;
 
-    if (!image || !video) { 
-        alert("Por favor, selecciona una imagen y un video antes de subir.");
-        return;
-    }
 
-    formData.append("imagen", image);
-    formData.append("video", video);
-
-    // Mostrar la imagen cargada en el contenedor de carga
-    previewImage.src = URL.createObjectURL(image);
-    formContainer.classList.add("hidden");
-    loadingDiv.classList.remove("hidden");
-
-    try {
-        const response = await fetch("http://127.0.0.1:8000/upload/", {
-            method: "POST",
-            body: formData,
-        });
-
-        const result = await response.json();
-
-        // Ocultar el contenedor de carga y mostrar los resultados
-        loadingDiv.classList.add("hidden");
-
-        imageResultDiv.innerHTML = `
-            <h3>Imagen Procesada:</h3>
-            <p>${result.imagen_resultado.resultados.map(res => `<br>Ubicación: ${res.ubicacion} - Mensaje: ${res.mensaje}`).join('')}</p>
-        `;
-
-        videoResultDiv.innerHTML = `
-            <h3>Detecciones en Video:</h3>
-            ${result.video_resultado.detecciones.map(det => `
-                <p>Frame: ${det.frame}, Ubicación: ${det.ubicacion}</p>
-            `).join('')}
-        `;
-
-        resultDiv.classList.remove("hidden");
-    } catch (error) {
-        console.error("Error al procesar los archivos:", error);
-        loadingDiv.classList.add("hidden");
-        formContainer.classList.remove("hidden");
-        alert("Ocurrió un error. Intenta nuevamente.");
-    }
+  } catch (error) {
+    console.error('Error al procesar los archivos:', error);
+    loadingDiv.classList.add('hidden');
+    alert('Ocurrió un error. Intenta nuevamente.');
+  }
 });
